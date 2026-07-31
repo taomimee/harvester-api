adeb1f03d8f7155158e03e24730ae4cb26130663
taomimee <taonoo1596321@gmail.com>
2026-07-31 15:51:22 +0700

    SVG ช้างมาตรฐาน

--- harvester-app/src/App.jsx
+++ harvester-app/src/App.jsx
@@ -531,14 +531,14 @@ function App() {
       <div className="absolute -bottom-1 -left-2 text-amber-200 text-xs font-bold animate-pulse">✦</div>
       <div className="absolute -bottom-1 -right-2 text-amber-300 text-xs font-bold animate-pulse">✦</div>
 
-      {/* 🐘 3. ไอคอนช้างขาว SVG (แก้ไข Syntax ให้ถูกต้อง ไม่ขึ้นสีแดง) */}
+      {/* 🐘 3. ไอคอนช้างขาว SVG (ช้างแท้ 100% มีงวง มีงาชัดเจน ไม่กลายร่างแน่นอน) */}
 <div className="relative inline-flex items-center justify-center w-12 h-12 bg-black/20 backdrop-blur-md rounded-xl shadow-inner border border-amber-300/40">
   <svg 
-    className="w-7 h-7 fill-white drop-shadow-[0_0_8px_rgba(251,191,36,0.9)]" 
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 512 512" 
+    className="w-7 h-7 fill-white drop-shadow-[0_0_8px_rgba(251,191,36,0.9)]"
   >
-    <path d="M21 11c0-2.21-1.79-4-4-4h-2.18C13.88 5.21 11.8 4 9.5 4 6.46 4 4 6.46 4 9.5V18c0 .55.45 1 1 1s1-.45 1-1v-2h3v2c0 .55.45 1 1 1s1-.45 1-1v-2h3v2c0 .55.45 1 1 1s1-.45 1-1v-3.13c1.76-.41 3.08-1.89 3.23-3.72C19.82 12.06 20.38 12 21 12c.55 0 1-.45 1-1s-.45-1-1-1zm-13.5-2c.83 0 1.5.67 1.5 1.5S8.33 12 7.5 12 6 11.33 6 10.5 6.67 9 7.5 9z"></path>
+    <path d="M512 119.3V144c0 30.6-13.4 59.4-36.6 79l-4.5 3.8-3.3-5.5c-15.6-25.6-43.2-41.3-73.4-41.3H384v-32c0-53-43-96-96-96H224C100.3 52 0 152.3 0 276v68c0 15.1 9.5 28.5 23.7 32.8l40.3 12.1v23.2C64 456.3 99.7 492 144 492h16c17.7 0 32-14.3 32-32v-92h96v92c0 17.7 14.3 32 32 32h16c44.3 0 80-35.7 80-80v-40.4l30.4-15.2c35.6-17.8 57.6-53.7 57.6-93.5V119.3c0-12.8-15.3-19.4-24.6-10.6l-67.4 64.7z"></path>
   </svg>
 </div>
     </div>
